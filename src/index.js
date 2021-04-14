f17dad25e619ab3c174cc3d72e6f5deef215832b
Ricardo Albor <ralbor@email.cloudnets.co>
2021-04-13 21:28:37 -0500

    change port server

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
-const port = 3000
+const port = 6006
 const publicDir = `${__dirname}/public`
 
 app
